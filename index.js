06c86c0190cdd8801caceb4f7ac985db53954ec9
Hossein <h.dvd13@gmail.com>
2023-03-12 16:43:28 -0700

    lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    const messages = [];
+    for (let i = 0; i < array.length; i++) {
+        messages[i] = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+    }
+    return messages;
+};
+
+function countDown(number) {
+    while (number > -1) {
+        console.log(number--);
+    }
+}
